76a2b8abd7d3fc08e91ae7e943dc770546a34a89
Infarh <shmachilin@yandex.ru>
2021-06-14 21:05:03 +0300

    Тестовые данные отправителей/получателей

--- MailSender/Data/TestData.cs
+++ MailSender/Data/TestData.cs
@@ -16,5 +16,25 @@ namespace MailSender.Data
                 UseSSL = i % 2 == 0
             })
            .ToList();
+
+        public static List<Sender> Senders { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Sender
+            {
+                Id = i,
+                Name = $"Отправитель - {i}",
+                Address = $"sender-{i}.server.ru",
+                Description = $"Описание отправителя {i}",
+            })
+           .ToList();
+
+        public static List<Recipient> Recipients { get; } = Enumerable.Range(1, 10)
+           .Select(i => new Recipient
+            {
+                Id = i,
+                Name = $"Получатель - {i}",
+                Address = $"recipient-{i}.server.ru",
+                Description = $"Описание получателя {i}"
+            })
+           .ToList();
     }
 }
