e73264296052f07f369ba07731ff54943e1739b4
Infarh <shmachilin@yandex.ru>
2021-06-17 20:35:39 +0300

    Базовый класс команды

--- Tests/MailSender.TestWPF/Commands/Base/Command.cs
+++ Tests/MailSender.TestWPF/Commands/Base/Command.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MailSender.TestWPF.Commands.Base
 {
-    public abstract class Command
+    public abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public virtual bool CanExecute(object parameter) => true;
+
+        public abstract void Execute(object parameter);
     }
 }
