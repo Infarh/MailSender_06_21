cb6a9c03892a4569787309170a5fce8483a38946
Infarh <shmachilin@yandex.ru>
2021-06-17 21:28:48 +0300

    Сервисы статистики и отправки почты

---
+++ MailSender.lib/Interfaces/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace MailSender.Interfaces
+{
+    public interface IMailService
+    {
+        void SendEmail(string From, string To, string Title, string Body);
+    }
+}
---
+++ MailSender.lib/Interfaces/IStatistic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MailSender.Interfaces
+{
+    public interface IStatistic
+    {
+        int SendedMailsCount { get; }
+
+        int SendersCount { get; }
+
+        int RecipientsCount { get; }
+
+        TimeSpan UpTime { get; }
+
+        void MessageSended();
+    }
+}
