8f718a9a3f23d107c3bd3db14230df97106f1d9a
Infarh <shmachilin@yandex.ru>
2021-06-17 20:23:27 +0300

    Базовый класс модели-представления

--- Tests/MailSender.TestWPF/ViewModels/MainWindowViewModel.cs
+++ Tests/MailSender.TestWPF/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,60 @@
-﻿using System.ComponentModel;
+﻿using MailSender.TestWPF.ViewModels.Base;
 
 namespace MailSender.TestWPF.ViewModels
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : ViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        //}
+
+        //protected bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        //{
+        //    if (Equals(field, value)) return false;
+        //    field = value;
+        //    OnPropertyChanged(PropertyName);
+        //    return true;
+        //}
 
         private string _Title = "Hello World!";
 
         public string Title
         {
             get => _Title;
+            //set
+            //{
+            //    if(Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //    OnPropertyChanged("TitleLength");
+            //}
             set
             {
-                _Title = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
+                if(Set(ref _Title, value))
+                    OnPropertyChanged(nameof(TitleLength));
             }
         }
+
+        public int TitleLength => _Title.Length;
+
+        private double _LeftPos;
+
+        public double LeftPos
+        {
+            get => _LeftPos;
+            set
+            {
+                if(Equals(_LeftPos, value)) return;
+                _LeftPos = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _TopPos;
+
+        public double TopPos { get => _TopPos; set => Set(ref _TopPos, value); }
     }
 }
