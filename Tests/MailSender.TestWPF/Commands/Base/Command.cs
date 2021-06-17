08870cf97de93c294d4af63522c3a15cbbac8250
Infarh <shmachilin@yandex.ru>
2021-06-17 20:34:08 +0300

    Тестовая команда

---
+++ Tests/MailSender.TestWPF/Commands/Base/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.TestWPF.Commands.Base
+{
+    public abstract class Command
+    {
+    }
+}
---
+++ Tests/MailSender.TestWPF/Commands/HelloCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MailSender.TestWPF.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private int _Counter = 3;
+
+        public bool CanExecute(object parameter) => _Counter > 0;
+
+        public void Execute(object parameter)
+        {
+            _Counter--;
+            MessageBox.Show("Всем привет!");
+        }
+    }
+}
