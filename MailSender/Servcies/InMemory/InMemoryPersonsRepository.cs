b98b835d6ea726c2afb0fde91e0b8cc359eeb4ee
Infarh <shmachilin@yandex.ru>
2021-06-21 19:53:55 +0300

    Репозиторий отправителей/получателей

---
+++ MailSender/Servcies/InMemory/InMemoryPersonsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MailSender.Models.Base;
+
+namespace MailSender.Servcies.InMemory
+{
+    public abstract class InMemoryPersonsRepository<T> : InMemoryRepository<T> where T : Person
+    {
+        protected InMemoryPersonsRepository(IEnumerable<T> items) : base(items) { }
+
+        public override void Update(T item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+
+            var db_item = GetById(item.Id);
+            if (db_item is null || ReferenceEquals(db_item, item)) return;
+
+            db_item.Name = item.Name;
+            db_item.Address = item.Address;
+            db_item.Description = item.Description;
+        }
+    }
+}
---
+++ MailSender/Servcies/InMemory/InMemoryRecipientsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MailSender.Models;
+
+namespace MailSender.Servcies.InMemory
+{
+    public class InMemoryRecipientsRepository : InMemoryPersonsRepository<Recipient>
+    {
+        private static IEnumerable<Recipient> GetTestData(int Count = 10) => Enumerable.Range(1, Count)
+           .Select(i => new Recipient
+            {
+                Name = $"Получатель {i}",
+                Address = $"recipient-{i}.server.ru",
+                Description = $"Описание получателя {i}"
+            });
+
+        public InMemoryRecipientsRepository() : base(GetTestData()) { }
+    }
+}
