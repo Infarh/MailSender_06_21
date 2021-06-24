e95678b88247cf2a482f4d8e6f99cc9cb6ceb79d
Infarh <shmachilin@yandex.ru>
2021-06-24 20:22:03 +0300

    Запуск и передача параметров в потоки

--- Tests/MailSender.TestConsole/Program.cs
+++ Tests/MailSender.TestConsole/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace MailSender.TestConsole
+﻿using System;
+using System.Threading;
+
+namespace MailSender.TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
+            ThreadsIntro.Run();
+
+            Console.WriteLine("Работа завершена. Нажмите Enter для выхода");
+            Console.ReadLine();
         }
     }
 }
