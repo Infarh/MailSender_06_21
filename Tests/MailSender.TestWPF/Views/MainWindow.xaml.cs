c59a610d8d2b7a2d45d3befdc9d085cc39d81784
Infarh <shmachilin@yandex.ru>
2021-06-28 21:25:58 +0300

    Работа с асинхрнными операциями в приложении с UI

--- Tests/MailSender.TestWPF/Views/MainWindow.xaml.cs
+++ Tests/MailSender.TestWPF/Views/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MailSender.TestWPF
 {
@@ -8,11 +10,63 @@ namespace MailSender.TestWPF
     {
         public MainWindow() => InitializeComponent();
 
-        private void Start_OnClick(object Sender, RoutedEventArgs E)
-        {
-            var thread_id = Thread.CurrentThread.ManagedThreadId;
+        //private void Start_OnClick(object Sender, RoutedEventArgs E)
+        //{
+        //    var thread_id = Thread.CurrentThread.ManagedThreadId;
 
-            new Thread(() => Calculate()) { IsBackground = true }.Start();
+        //    new Thread(() => Calculate()) { IsBackground = true }.Start();
+        //}
+
+        private void Cancel_OnClick(object Sender, RoutedEventArgs E)
+        {
+            _Cancellation?.Cancel();
+        }
+
+        private CancellationTokenSource _Cancellation;
+        private async void Start_OnClick(object Sender, RoutedEventArgs E)
+        {
+            var button = (Button) Sender;
+            button.IsEnabled = false;
+            CancelButton.IsEnabled = true;
+
+            var cancellation = new CancellationTokenSource();
+            _Cancellation = cancellation;
+
+            var progress = new Progress<double>(p => Progress.Value = p * 100);
+            try
+            {
+                var result = await CalculateAsync(Progress: progress, Cancel: cancellation.Token);
+                Result.Text = result.ToString();
+            }
+            catch (OperationCanceledException)
+            {
+                Result.Text = "Отмена!";
+                Progress.Value = 0;
+            }
+
+            button.IsEnabled = true;
+            CancelButton.IsEnabled = false;
+        }
+
+        private static async Task<int> CalculateAsync(int Max = 100, int Timeout = 100, IProgress<double> Progress = null, CancellationToken Cancel = default)
+        {
+            //var thread_id = Thread.CurrentThread.ManagedThreadId;
+
+            await Task.Delay(Timeout, Cancel).ConfigureAwait(false);
+
+            //var thread2_id = Thread.CurrentThread.ManagedThreadId;
+
+            var s = 0;
+            for (var i = 1; i <= Max && !Cancel.IsCancellationRequested; i++)
+            {
+                s += i;
+                Progress?.Report((double)i / Max);
+                await Task.Delay(Timeout, Cancel).ConfigureAwait(false);
+            }
+            Cancel.ThrowIfCancellationRequested();
+
+            Progress?.Report(1);
+            return s;
         }
 
         private void Calculate(int Max = 100, int Timeout = 100)
@@ -41,7 +95,7 @@ namespace MailSender.TestWPF
                 {
                     Result.Text = s.ToString();
                 });
-            
+
         }
     }
 }
