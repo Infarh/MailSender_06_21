834ed33df96836cb6c3a9fab751af20055fc296d
Infarh <shmachilin@yandex.ru>
2021-07-01 20:33:33 +0300

    Описание сущностей структуры БД

---
+++ Tests/MailSender.TestConsole/Entityes/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MailSender.TestConsole.Entityes
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        //public Student Student { get; set; }
+
+        public ICollection<Student> Students { get; set; } = new HashSet<Student>();
+    }
+}
