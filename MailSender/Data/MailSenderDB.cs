67d62618d4ffa997caa93e34993db432979ae400
Infarh <shmachilin@yandex.ru>
2021-07-01 21:29:12 +0300

    Структура контекста БД главного приложения

---
+++ MailSender/Data/MailSenderDB.cs
@@ -0,0 +1,22 @@
+﻿using MailSender.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailSender.Data
+{
+    public class MailSenderDB : DbContext
+    {
+        public DbSet<Server> Servers { get; set; }
+
+        public DbSet<Sender> Senders { get; set; }
+
+        public DbSet<Recipient> Recipients { get; set; }
+
+        public DbSet<EmailsList> EmailsLists { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<SchedulerTask> SchedulerTasks { get; set; }
+
+        public MailSenderDB(DbContextOptions<MailSenderDB> options) : base(options) { }
+    }
+}
