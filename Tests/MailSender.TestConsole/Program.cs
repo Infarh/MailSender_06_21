b214d4ff9719f60302f703759af844594bff79f4
Infarh <shmachilin@yandex.ru>
2021-07-05 19:57:47 +0300

    О рефлексии

--- Tests/MailSender.TestConsole/Program.cs
+++ Tests/MailSender.TestConsole/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace MailSender.TestConsole
+﻿using System;
+using System.Reflection;
+
+namespace MailSender.TestConsole
 {
     class Program
     {
+        //public string this[int index]
+        //{
+        //    get { }
+        //    set { }
+        //}
+
         static void Main(string[] args)
         {
-            
+            //Type program_type = typeof(Program);
+            //Type program = new Program();
+            //Type program_type2 = program.GetType().FullName;
+
+            //Assembly asm;
+            //Module module;
+            //Type type;
+
+            //MemberInfo member;
+            //ConstructorInfo ctor;
+            //MethodInfo method;
+            //PropertyInfo property;
+            //FieldInfo field;
+            //EventInfo @event;
+
+            //ParameterInfo parameter;
         }
     }
 }
